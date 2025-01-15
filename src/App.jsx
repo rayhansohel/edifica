14255835056136259e70960b4c90e5118d3337c3
RayhanSohel <arayhansohel@gmail.com>
2025-01-15 21:34:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="font-poppins flex justify-center items-center h-screen w-full">
+        <h1>Hello Edifica</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
