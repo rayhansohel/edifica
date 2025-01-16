0515761429f1a20701744a9a387f508eb2f54380
RayhanSohel <arayhansohel@gmail.com>
2025-01-16 15:35:57 +0600

    Add folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="font-poppins flex justify-center items-center h-screen w-full">
+      <div className="font-poppins text-sm flex justify-center items-center h-screen w-full">
         <h1>Hello Edifica</h1>
       </div>
     </>
